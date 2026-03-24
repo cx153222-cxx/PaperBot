0390f466376545d48094a9355f37df41e689f50f
cx153222-cxx <cx153222@gmail.com>
2026-03-24 15:20:11 +0800

    Update arxiv_fetch.py

--- arxiv_fetch.py
+++ arxiv_fetch.py
@@ -1,8 +1,9 @@
 import arxiv
 
-qu = '(cat:cs.NE OR cat:math.OC OR cat:cs.LG) AND (all:"surrogate-assisted" OR all:"Kriging" OR all:"black-box optimization")'
+# qu = '(cat:cs.NE OR cat:math.OC OR cat:cs.LG) AND (all:"surrogate-assisted" OR all:"Kriging" OR all:"black-box optimization")'
+CURRENT_QUERY = os.getenv("ARXIV_QUERY", "cs.IR")
 
-def get_arxiv_papers(query=qu, limit=10):
+def get_arxiv_papers(query=qu, limit=7):
     search = arxiv.Search(
         query=query,
         max_results=limit,
